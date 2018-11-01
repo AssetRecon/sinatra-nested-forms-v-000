3a6492e3bee0936f1ef5dd2341cba08f9b70deab
Edsel Caprice <caprice@mail.usf.edu>
2018-11-01 20:24:53 +0000

    automatically backed up by learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,16 +1,17 @@
 class Pirate
-  attr_reader :name, :weight, :height
 
-  PIRATES = []
+  attr_accessor :name, :weight, :height
+  @@all = []
 
-  def initialize(args)
-    @name = args[:name]
-    @weight = args[:weight]
-    @height = args[:height]
-    PIRATES << self
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
   end
 
   def self.all
-    PIRATES
+    @@all
   end
+
 end
