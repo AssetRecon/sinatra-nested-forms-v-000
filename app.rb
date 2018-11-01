1f6c51c5e9f2daefd59d67a06346bb383ae48879
Edsel Caprice <caprice@mail.usf.edu>
2018-11-01 19:53:00 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,10 +9,22 @@ module FormsLab
     end
 
     post '/priates' do
-
+      @pirate = Priate.new(params[])
 
       erb :show
     end
+
+    post '/student' do
+  @student = Student.new(params[:student])
+
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+
+  @courses = Course.all
+
+  erb :student
+end
 
   end
 end
